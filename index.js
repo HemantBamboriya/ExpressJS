18bfd1c1030c3cbbbcef408bc9832ec8f9033df1
Hemant Bamboriya <your-email@example.com>
2025-05-20 20:23:05 +0530

    after logger

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 const app= express()
-const port=process.env.PORT ||3000
+const port=3000
+//const port = process.env.PORT || 3000;
 
 // app.get("/",(req,res)=>{
 //     res.send("I will learn backend easily baby !....")
@@ -22,7 +25,22 @@ const port=process.env.PORT ||3000
 app.use(express.json())
 let teaData = []
 let nextId=1
-
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 //add a new tea
 
 app.post('/teas',(req,res)=>{
